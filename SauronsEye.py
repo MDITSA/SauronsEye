f81104333ee489fd1dffe22c3811d62e96c6cc2e
Mario Dietlein <github@mditsa.de>
2021-11-30 13:23:41 +0100

    Fixed webcam access under linux. At least for my webcam. Updated requirements.txt with new versions tested.

--- SauronsEye.py
+++ SauronsEye.py
@@ -1,8 +1,9 @@
 # Following modules are needed:
-# pygame, opencv-python, face_recognition
+# platform(should be already installed), pygame, opencv-python, face_recognition
 #
-# Every other module in the requirements.txt is a automatically installed dependency with pip (as far as i remember cmake is needed to for dlib)
+# Every other module in the requirements.txt is a automatically installed dependency (as far as i remember cmake is needed to for dlib, which is needed for face_recognition)
 
+import platform
 import pygame
 import cv2
 import face_recognition
@@ -12,8 +13,13 @@ pygame.init()
 class SauronsEye(object):
     def __init__(self):
         pygame.display.set_caption('SauronsEye')
+        self.platform = platform.system()
 
-        self.video_capture = cv2.VideoCapture(1)
+        if self.platform == 'Windows' :
+            self.video_capture = cv2.VideoCapture(1) #Adjust depending on webcam settings
+        elif self.platform == 'Linux' :
+            self.video_capture = cv2.VideoCapture(8, cv2.CAP_V4L2) #Adjust depending on webcam settings
+        
         self.video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
         self.video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 270)
         self.face_locations = []
@@ -21,7 +27,7 @@ class SauronsEye(object):
         self.display_surface = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.display_width, self.display_height = self.display_surface.get_size()
 
-        self.image = pygame.image.load(r'media\eye.png').convert_alpha()
+        self.image = pygame.image.load(r'media/eye.png').convert_alpha()
         self.image_width, self.image_height = self.image.get_size()
 
         pygame.mixer.music.load('media/voice.mp3')
@@ -48,7 +54,8 @@ class SauronsEye(object):
             self.face_center_x = (self.face_left_resized+self.face_right_resized)/2
             self.face_center_y = (self.face_top_resized+self.face_bottom_resized)/2
 
-            self.image_pos_x = 1920-self.face_center_x-(self.image_width/4) #1920- needed because otherwise the movement would be inverted... dont ask me why...
+            #self.image_pos_x = 1920-self.face_center_x-(self.image_width/4) #1920- needed because otherwise the movement would be inverted on a monitor... dont ask me why...
+            self.image_pos_x = self.face_center_x-(self.image_width/2)
             self.image_pos_y = self.face_center_y-(self.image_height/2)
 
     def show_eye(self):
@@ -61,7 +68,7 @@ class SauronsEye(object):
         pygame.mixer.music.play(0,0.0)
 
     def main(self): 
-        while True:
+        while 1:
             for event in pygame.event.get() :
                 if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE :
                     pygame.display.quit()
@@ -72,7 +79,7 @@ class SauronsEye(object):
             self.show_eye()
 
             self.now = pygame.time.get_ticks()
-            if self.now > self.tick + 20000:
+            if self.now > self.tick + 25000:
                 self.play_voice()
                 self.tick = self.now
 
